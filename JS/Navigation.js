cb510e3447542051301ef563b5d067fdba7ac5f4
Matipa Marufu <2344503@students.wits.ac.za>
2022-08-05 18:45:03 +0200

    creating basic files

---
+++ JS/Navigation.js
@@ -0,0 +1,35 @@
+
+function Index()
+{
+    location.href="Index.html";
+}
+
+function BlogPostSection()
+{
+    location.href="BlogPostSection.html";
+}
+
+function BlogPost1()
+{
+    location.href="BlogPost1.html";
+}
+
+function BlogPost2()
+{
+    location.href="BlogPost2.html";
+}
+
+function DesignSection()
+{
+    location.href="DesignSection.html";
+}
+
+function DataVisualisationSection()
+{
+    location.href="DataVisualisationSection.html";
+}
+
+function DataArt()
+{
+    location.href="DataVisualisationSection.html";
+}
