fa371ed489f47c03d4e6a1bcd53896f20bd20111
Matipa Marufu <2344503@students.wits.ac.za>
2022-09-11 05:33:23 +0200

    working on p text for mobile

--- JS/api.js
+++ JS/api.js
@@ -15,3 +15,19 @@ const handleStudioGhibli = (e) => { //when user clicks on point should display i
     document.querySelector(".text-2b").innerText = e.description;
     document.querySelector(".img-1").src = e.image;
 }
+
+
+// fetch('https://api.metalpriceapi.com/v1/latest?api_key=efb5b25b8921524e44968d5c2ca45a7c&base=USD&currencies=XAU,XAG,XPT,XPD,XRH,RUTH,IRD,OSMIUM,')
+// .then(r => 
+//     {return r.json()})
+// .then(p => {
+//     // {for (const a of data){
+//     //     console.log(a.rates)
+//     // }}
+//     console.log(p.parse());
+//     console.log(p.parse().rates);
+// })
+// .catch(
+//     error => console.warn('Our warning',error));
+
+
