c6d7f4a60bbf90a8a887b5af217eafd63434d9a0
Matipa Marufu <2344503@students.wits.ac.za>
2022-11-03 09:25:27 +0200

    JS navigation

--- JS/Navigation.js
+++ JS/Navigation.js
@@ -1,85 +1,211 @@
-let navMenu = ["index.html", "BlogPostSection.html", "DesignSection.html", "Data.html", "References.html"];
-let navMenuItems = [document.getElementById('index'), document.getElementById('index'), document.getElementById('index'), document.getElementById('index'), document.getElementById('index')] 
-let i =0; //index shoud be required destination
+//NAV BAR
+let navMenu = ["index.html", "BlogPostSection.html", "DesignSection.html", "Data.html"];
+let navArea = document.getElementById('nav');
+let navButtons = navArea.getElementsByTagName('a');
+let icons = navArea.getElementsByTagName('i');
 
-navMenuItems[i].onclick = function()
+for(let i = 0; i < navMenu.length; i++)
 {
-    location.href = navMenu[i]
+    navButtons[i].classList.add("u-url");
+    icons[i].classList.add("fa-solid");
+
+    let className;
+    let innerText;
+
+    switch(i)
+    {
+        case 0:
+            className = "fa-house-chimney";
+            innerText = "Home";
+            break;
+        case 1:
+            className = "fa-blog";
+            innerText = "Blogs";
+            break;
+        case 2:
+            className = "fa-paint-roller";
+            innerText = "Design";
+            break;
+        case 3:
+            className = "fa-database";
+            innerText = "Data";
+            break;
+    }
+
+    icons[i].classList.add(className);
+    icons[i].innerHTML = innerText;
+
+    navButtons[i].onclick = function()
+    {
+        location.href = navMenu[i]
+    }
 }
 
-function Index()
-{
-    location.href="index.html";
-};
+LoadPages();
 
-function BlogPostSection()
+// //in the situation the page is loaded directly and not via the section pages
+function LoadPages()
 {
-    location.href="BlogPostSection.html";
-};
 
-function BlogPost1()
-{
-    location.href="BlogPost1.html";
-};
+    let blogPages = ["BlogPost1.html", "BlogPost2.html", "BlogPost3.html"];
+    let dataPages = ["DataVisualisationSection.html", "DataArt.html"];
+    let designPages = ["DesignWireframes.html", "RevisedWireframes.html", "DesignDocumentation.html", "DataArtDocumentation.html", "InteractiveDataDocumentation.html"];
 
-function BlogPost2()
-{
-    location.href="BlogPost2.html";
-};
+    let selectedArray = [];
+    let designPagesselectedGridName;
+    let currentButtons;
 
-function BlogPost3()
-{
-    location.href="BlogPost3.html";
-};
+    let path = window.location.pathname;
+    let pageName = path.split("/").pop();
+    console.log(pageName);
 
-function DesignSection()
-{
-    location.href="DesignSection.html";
-};
 
-function DataVisualisationSection()
-{
-    location.href="DataVisualisationSection.html";
-};
+    for(let a = 0; a < blogPages.length; a++)
+    {
+        if((pageName == blogPages[a]) || (pageName == "BlogPostSection.html"));
+        {
+            selectedArray = blogPages;
+            selectedGridName = "blog-grid"
+        }
+    }
 
-function DataArt()
-{
-    location.href="DataArt.html";
-};
+    for(let b = 0; b < designPages.length; b++)
+    {
 
-function DataArtDocumentation()
-{
-    location.href="DataArtDocumentation.html";
-} 
+        if((pageName == designPages[b]) || (pageName == "DesignSection.html")){
+            console.log(pageName == designPages[b]);
+            console.log(pageName == "DesignSection.html");
+            selectedArray = designPages;
+            selectedGridName = "design-grid"
+        }else{
 
-function Data()
-{
-    location.href="Data.html";
-};
+        }
+    }
 
-function DesignWireframes()
-{
-    location.href="DesignWireframes.html";
-};
+    for(let c = 0; c < dataPages.length; c++)
+    {
 
-function DesignDocumentation()
-{
-    location.href="DesignDocumentation.html";
-};
+        if((pageName == dataPages[c]) || (pageName == "Data.html"))
+        {
+            console.log(pageName == dataPages[c]);
+            console.log(pageName == "Data.html");
+            selectedArray = dataPages;
+            selectedGridName = "data-grid";
+        }
+        else
+        {
+        }
+    } 
 
-function InteractiveDataDocumentation()
-{
-    location.href="InteractiveDataDocumentation.html";
-};
+    if(selectedGridName != null)
+    {
+        console.log(selectedGridName);
+            let selectedGrid = document.getElementById(selectedGridName);
+            console.log(selectedArray);
+      
+            for(let i = 0; i < selectedArray.length; i++)
+        {
+            if(selectedGrid != null)
+            {
+                currentButtons = selectedGrid.getElementsByTagName('a');
+
+                currentButtons[i].classList.add("u-url");
+                console.log(currentButtons);
+        
+                currentButtons[i].onclick = function()
+                {
+                    console.log(currentButtons[i]);
+                    location.href = selectedArray[i]
+                }
+            }
+
+
+            // not working            
+            if(pageName == selectedArray[i])
+            {
+                console.log("on" + {pageName} + "page");
+        
+                let gridBtns = document.getElementById('grid-btns');
+                let buttons = gridBtns.getElementsByTagName('a'); //array
+                console.log(i);
+        
+                buttons[1].onclick = function()
+                {
+                    location.href = selectedArray[i]
+                }
+        
+                for(let j = 0; j < buttons.length; j++)
+                {
+                    buttons[j].classList.add("u-url"); 
+                    let dataClassname;
+                    let buttonsText; 
+        
+                    switch(j)
+                    {
+                        case 0:
+                            dataClassname = "prev-btn";
+                            buttonsText = "Prev."
+                            break;
+                        case 1:
+                            dataClassname = "top-btn";
+                            buttonsText = "Top."
+                            break;
+                        case 2: 
+                            dataClassname = "next-btn";
+                            buttonsText = "Next"
+                            break;
+                    }
+                    buttons[j].classList.add(dataClassname); 
+                    buttons[j].innerHTML = buttonsText;
+                }
+        
+                if((i+1)>= selectedArray.length)
+                {
+                    buttons[0].onclick = function()
+                    {
+                        location.href = selectedArray[i-1]
+                    }
+        
+                    buttons[2].onclick = function() //dont hard code numbers!
+                    {
+                        location.href = selectedArray[0]
+                    }
+                }
+                else if((i-1) < 0)
+                {
+                    buttons[0].onclick = function()
+                    {
+                        location.href = selectedArray[selectedArray.length-1]
+                    }
+        
+                    buttons[2].onclick = function()
+                    {
+                        location.href = selectedArray[i + 1]
+                    }
+                }
+                else{
+        
+                    buttons[0].onclick = function()
+                    {
+                        location.href = selectedArray[i-1]
+                    }
+        
+                    buttons[2].onclick = function()
+                    {
+                        location.href = selectedArray[i + 1]
+                    }
+                }
+                
+                
+            }
+        }
+    }
+
+}   
 
 function References()
 {
     location.href="References.html";
-};
-
-function RevisedWireframes()
-{
-    location.href="RevisedWireframes.html";
 };
 
 window.onresize = function(){ location.reload(); }
